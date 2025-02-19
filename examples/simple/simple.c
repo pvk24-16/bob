d49761b2409b5f2a6e6242243907d75973408571
bobpvk <195304264+bobpvk@users.noreply.github.com>
2025-02-19 15:26:02 +0100

    add windows support for 'simple' example

--- examples/simple/simple.c
+++ examples/simple/simple.c
@@ -1,29 +1,36 @@
 #include <stdlib.h>
 #include <bob.h>
 
-struct bob_api api;
+#ifdef WIN32
+#define EXPORT __attribute__((dllexport))
+#else
+#define EXPORT
+#endif
 
-static struct bob_visualization_info info;
+EXPORT struct bob_api api;
 
-const struct bob_visualization_info *get_info(void)
+static struct bob_visualization_info info = {
+  .name = "Simple example",
+  .description = "This is a description that is not very useful",
+  .enabled = BOB_AUDIO_CHROMAGRAM_STEREO | BOB_AUDIO_TEMPO_MONO,
+};
+
+EXPORT const struct bob_visualization_info *get_info(void)
 {
-  info.name = "Simple example";
-  info.description = "This is a description that is not very useful";
-  info.enabled = BOB_AUDIO_CHROMAGRAM_STEREO | BOB_AUDIO_TEMPO_MONO;
   return &info;
 }
 
-void *create(void)
+EXPORT void *create(void)
 {
   return NULL;
 }
 
-void update(void *userdata)
+EXPORT void update(void *userdata)
 {
   (void) userdata;
 }
 
-void destroy(void *userdata)
+EXPORT void destroy(void *userdata)
 {
   (void) userdata;
 }
