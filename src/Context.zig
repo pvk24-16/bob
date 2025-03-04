56ec72a6b7d106b1befd12eb88130a5897a37c20
bobpvk <195304264+bobpvk@users.noreply.github.com>
2025-03-04 14:34:22 +0100

    add AudioCapturer to Context and add application selection in GUI

--- src/Context.zig
+++ src/Context.zig
@@ -6,17 +6,26 @@ const Context = @This();
 
 const GuiState = @import("GuiState.zig");
 const Client = @import("Client.zig");
+const AudioCapturer = @import("audio/capture.zig").AudioCapturer;
 
 gui_state: GuiState,
 client: ?Client,
+capturer: ?AudioCapturer,
+allocator: std.mem.Allocator,
 
 pub fn init(allocator: std.mem.Allocator) Context {
     return .{
         .gui_state = GuiState.init(allocator),
         .client = null,
+        .capturer = null,
+        .allocator = allocator,
     };
 }
 
 pub fn deinit(self: *Context) void {
     self.gui_state.deinit();
+    if (self.client) |*client|
+        client.unload();
+    if (self.capturer) |*capturer|
+        capturer.deinit(self.allocator);
 }
