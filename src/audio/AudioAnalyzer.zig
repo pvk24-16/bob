cb94d46d3e40931593591cc1db9e566f461f1bce
Simon Jutwreten <simonju@kth.se>
2025-03-13 11:22:16 +0100

    Add Audio Analyzer WIP

--- src/audio/AudioAnalyzer.zig
+++ src/audio/AudioAnalyzer.zig
@@ -1,10 +1,20 @@
 const std = @import("std");
 const AudioAnalyzer = @This();
 
-const AudioCapturer = @import("AudioCapturer.zig");
-const AudioSplixer = @import("AudioSplixer.zig");
 const FFT = @import("fft.zig").FastFourierTransform;
 
-capturer: AudioCapturer,
-splixer: AudioSplixer,
-fft: FFT,
+spectral_analyzer: FFT,
+
+pub fn init(allocator: std.mem.Allocator) !AudioAnalyzer {
+    const spectral_analyzer = try FFT.init(std.math.log2_int_ceil(usize, 4096), 2, .blackman_nuttall, 0.8, allocator);
+    errdefer spectral_analyzer.deinit(allocator);
+
+    return AudioAnalyzer{
+        .spectral_analyzer = spectral_analyzer,
+    };
+}
+
+pub fn deinit(self: *AudioAnalyzer, allocator: std.mem.Allocator) void {
+    self.spectral_analyzer.deinit(allocator);
+    self.* = undefined;
+}
