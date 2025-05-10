48a891b433da0c49d39d444f67213fdf6832b12d
Nickname <63918223+zanli08@users.noreply.github.com>
2025-05-10 23:57:47 +0200

    actual bugfix for linux

--- examples/fountain/startFountain.py
+++ examples/fountain/startFountain.py
@@ -4,6 +4,6 @@ import platform
 if platform.system() == 'Windows':
     subprocess.call(['..\\..\\..\\examples\\fountain\\visualizern\\win\\Fountain.exe'])
 elif platform.system() == 'Linux':
-    subprocess.call(['./examples/fountain/linux/Fountain.x86_64'])
+    subprocess.call(['../../../examples/fountain/visualizern/linux/Fountain.x86_64'])
 elif platform.system() == 'Darwin':
     print('\033[91m Fountain is not supported on macOS! \033[00m')
